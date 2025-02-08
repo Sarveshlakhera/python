e4a81dab7cc2a9f8f2b4c8b335a450a1c568d43e
sarveshlakhera <sarveshlakhera@gmail.com>
2025-02-08 23:56:12 +0530

    Initial practice

---
+++ python-practice/Variables.py
@@ -0,0 +1,9 @@
+name = "Sarvesh Lakhera"
+age = 25
+city = "New York"
+print(name + " is my name.", "I am " + str(age) + " old.", "I live in " + city)
+
+# Python offers a powerful feature called f-strings (formatted string literals) to simplify 
+# string formatting and interpolation. f-strings is introduced in Python 3.6 it provides a
+# concise and intuitive way to embed expressions and variables directly into strings.
+print(f"{name} is my name.", f"I am {age} old.", f"I live in {city}")
