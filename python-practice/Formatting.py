dc41c099bf57d7b516a9c8aef252a4468596aa67
sarveshlakhera <sarveshlakhera@gmail.com>
2025-02-10 22:43:32 +0530

    taking input in python

---
+++ python-practice/Formatting.py
@@ -0,0 +1,12 @@
+# end Parameter with '@'
+print("Python", end='@')
+print("GeeksforGeeks")
+
+# Seprating with Comma
+print('D', 'A', 'V', sep='.')
+
+# for formatting a date
+print('09', '12', '2016', sep='/')
+
+# another example
+print('Sarvesh', 'gmail.com', sep='@')
---
+++ python-practice/Input.py
@@ -0,0 +1,19 @@
+# Taking input from the user
+num = int(input("Enter a value: "))
+
+# Adding
+add = num + 5
+
+# Output
+print("The sum is %d" %add)
+
+# taking two inputs at a time
+x, y = input("Enter two values: ").split()
+print("Number of boys: ", x)
+print("Number of girls: ", y)
+ 
+# taking three inputs at a time
+x, y, z = input("Enter three values: ").split()
+print("Total number of students: ", x)
+print("Number of boys is : ", y)
+print("Number of girls is : ", z)
